b4ef4bc1622f638220d1d5844b2038b636cbba93
bwgypyth <bwgypyth@gmx.net>
2013-02-07 23:07:45 +0100

    Format code, no logical changes

--- LOTRO-Tools/LOTROendecryptClient/Decrypt.cs
+++ LOTRO-Tools/LOTROendecryptClient/Decrypt.cs
@@ -7,82 +7,82 @@ using System.Collections;
 
 namespace LOTROendecryptClient
 {
-    
-    class Decrypt
-    {
 
-        private FileStream fs;
-        private int[,] jumpTable;
-        private List<byte[][]> lookUpList;
-        private int startPosition;
-        private int lastIndex = 0; // last index has to be keept til new value is found
+	class Decrypt
+	{
+
+		private FileStream fs;
+		private int[,] jumpTable;
+		private List<byte[][]> lookUpList;
+		private int startPosition;
+		private int lastIndex = 0; // last index has to be keept til new value is found
 
 
-        public Decrypt(FileStream fs)
-        {
-            this.fs = fs;
-            this.jumpTable = HelperMethods.Instance.getJumpTable();
-            this.lookUpList = HelperMethods.Instance.getLookUpList();
-        }
+		public Decrypt(FileStream fs)
+		{
+			this.fs = fs;
+			this.jumpTable = HelperMethods.Instance.getJumpTable();
+			this.lookUpList = HelperMethods.Instance.getLookUpList();
+		}
 
-        public byte[] generateDecryptedPacket()
-        {
-            // Shouldn't happen
-            if (fs == null)
-                return null;
+		public byte[] generateDecryptedPacket()
+		{
+			// Shouldn't happen
+			if (fs == null)
+				return null;
 
-            byte[] tempResult = new byte[fs.Length * 4];
-            int pos = 0;
-            startPosition = 4; // the first 4 Bits in the first block are skiped
+			byte[] tempResult = new byte[fs.Length * 4];
+			int pos = 0;
+			startPosition = 4; // the first 4 Bits in the first block are skiped
 
-            // Returns the final decrypted packet
-            byte[] decryptedPacket = null;
+			// Returns the final decrypted packet
+			byte[] decryptedPacket = null;
 
-            // First 2 Bytes are the same in the decrypted packet
-            byte firstByte = (byte) fs.ReadByte();
-            byte secondByte = (byte) fs.ReadByte();
+			// First 2 Bytes are the same in the decrypted packet
+			byte firstByte = (byte) fs.ReadByte();
+			byte secondByte = (byte) fs.ReadByte();
 
-            // Write them to temp array
-            tempResult[0] = firstByte;
-            tempResult[1] = secondByte;
-            pos = 2;
+			// Write them to temp array
+			tempResult[0] = firstByte;
+			tempResult[1] = secondByte;
+			pos = 2;
 
-            // Decrypt blocks of 4 Byte
-            long lengthPacket = fs.Length - 2; // first 2 Bytes already read and have nothing to do with the decrypted packet
-            int numberOfBlocks = (int)lengthPacket / 4;
-            int lastBlockLength = (int)lengthPacket % 4;
+			// Decrypt blocks of 4 Byte
+			long lengthPacket = fs.Length - 2; // first 2 Bytes already read and have nothing to do with the decrypted packet
+			int numberOfBlocks = (int)lengthPacket / 4;
+			int lastBlockLength = (int)lengthPacket % 4;
 
-            byte[] block = new byte[4];
-            byte[] lastBlock = new byte[lastBlockLength];
+			byte[] block = new byte[4];
+			byte[] lastBlock = new byte[lastBlockLength];
 
-            for (int i = 0; i < numberOfBlocks; i++)
-            {
-                fs.Read(block, 0, 4);
-                byte[] decryptedBlock = processBlock(block);
-                Buffer.BlockCopy(decryptedBlock, 0, tempResult, pos, decryptedBlock.Length);
-                pos += decryptedBlock.Length;
-                startPosition = 0;
+			for (int i = 0; i < numberOfBlocks; i++)
+			{
+				fs.Read(block, 0, 4);
+				byte[] decryptedBlock = processBlock(block);
+				Buffer.BlockCopy(decryptedBlock, 0, tempResult, pos, decryptedBlock.Length);
+				pos += decryptedBlock.Length;
+				startPosition = 0;
 
-            }
+			}
 
-            // Decrypt last block
-            if (lastBlockLength > 0)
-            {
+			// Decrypt last block
+			if (lastBlockLength > 0)
+			{
 
-                fs.Read(lastBlock, 0, lastBlockLength);
-                byte[] decryptedBlock = processBlock(lastBlock);
-                Buffer.BlockCopy(decryptedBlock, 0, tempResult, pos, decryptedBlock.Length);
-                pos += decryptedBlock.Length;
-            }
+				fs.Read(lastBlock, 0, lastBlockLength);
+				byte[] decryptedBlock = processBlock(lastBlock);
+				Buffer.BlockCopy(decryptedBlock, 0, tempResult, pos, decryptedBlock.Length);
+				pos += decryptedBlock.Length;
+			}
 
-            decryptedPacket = new byte[pos];
-            Buffer.BlockCopy(tempResult, 0, decryptedPacket, 0, pos);
+			decryptedPacket = new byte[pos];
+			Buffer.BlockCopy(tempResult, 0, decryptedPacket, 0, pos);
 
-            return decryptedPacket;
+			return decryptedPacket;
 
-        }
+		}
 
-        /*  This is how the decryption is done:
+		/*  This is how the decryption is done:
          * 1. Revers order of the 4 Bytes (Example: 0xEF 0xF5 0x49 0x85 => 0x85 0x49 0xF5 0xEF)
          * 2. Get bit value of the 4 Byte word (Example:  0x85 0x49 0xF5 0xEF => 10000101010010011111010111101111)
          * 3. Revers the order again (Example: 10000101010010011111010111101111 => 11110111101011111001001010100001)
@@ -97,48 +97,48 @@ namespace LOTROendecryptClient
          * 5. Get the value which is given in the second look-up-table
          * 6. Start with 4 again til there are no bits left in the array
          */
-        private byte[] processBlock(byte[] block)
-        {
-            byte[] tempResult = new byte[block.Length * 16];
-            int pos = 0;
+		private byte[] processBlock(byte[] block)
+		{
+			byte[] tempResult = new byte[block.Length * 16];
+			int pos = 0;
 
-            //Array.Reverse(block); BitArray is reversing bytes default
+			//Array.Reverse(block); BitArray is reversing bytes default
 
-            BitArray bitArray = new BitArray(block);
-          
-            int index = 0;
+			BitArray bitArray = new BitArray(block);
 
-            for (int i = startPosition; i < bitArray.Length; i++)
-            {
-                int column = bitArray.Get(i) ? 1 : 0;
+			int index = 0;
 
-                if (jumpTable[lastIndex, column] >= 0)
-                {
-                    lastIndex = jumpTable[lastIndex, column];
-                }
-                else
-                {
-                    index = jumpTable[lastIndex, column] + 16372;
+			for (int i = startPosition; i < bitArray.Length; i++)
+			{
+				int column = bitArray.Get(i) ? 1 : 0;
 
-                    byte[][] lookUpEntry = lookUpList[index];
-                    byte[] lookUpValue = lookUpEntry[0];
+				if (jumpTable[lastIndex, column] >= 0)
+				{
+					lastIndex = jumpTable[lastIndex, column];
+				}
+				else
+				{
+					index = jumpTable[lastIndex, column] + 16372;
 
-                    Buffer.BlockCopy(lookUpValue, 0, tempResult, pos, lookUpValue.Length);
+					byte[][] lookUpEntry = lookUpList[index];
+					byte[] lookUpValue = lookUpEntry[0];
 
-                    pos += lookUpValue.Length;
+					Buffer.BlockCopy(lookUpValue, 0, tempResult, pos, lookUpValue.Length);
 
-                    lastIndex = 0;
-                }                  
+					pos += lookUpValue.Length;
 
-           }
+					lastIndex = 0;
+				}                  
 
-            byte[] result = new byte[pos];
+			}
 
-            Buffer.BlockCopy(tempResult, 0, result, 0, pos);
+			byte[] result = new byte[pos];
 
-            return result;
-        }
+			Buffer.BlockCopy(tempResult, 0, result, 0, pos);
+
+			return result;
+		}
 
 
-    }
+	}
 }
