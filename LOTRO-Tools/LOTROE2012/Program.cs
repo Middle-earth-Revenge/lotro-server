6dcbb1342d4bf9536f0d917203d295c5be7df5b1
bwgypyth <bwgypyth@gmx.net>
2013-03-02 13:40:50 +0100

    Add LOTROE2012 project

---
+++ LOTRO-Tools/LOTROE2012/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+using System.IO;
+using System.Net.Sockets;
+using Helper;
+using System.Collections;
+
+
+
+
+
+namespace LOTROE2012
+{
+    class Program
+    {
+        private ConsoleKeyInfo cki;
+
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.TreatControlCAsInput = true;
+
+            bool configFound = false;
+
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Missing config file name!");
+                System.Console.WriteLine("Please start the server with a valid config file.");
+                System.Console.WriteLine();
+                System.Console.WriteLine("LOTRO-SE.exe {config file}");
+                System.Console.WriteLine();
+                System.Console.WriteLine("e.g. > LOTRO-SE.exe config.xml");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+            }
+
+            Settings.ConfigHandler configHandler = new Settings.ConfigHandler();
+            configHandler.readConfig(args[0]);
+
+           if (Settings.Config.Instance.Debug)
+            {
+                TextWriterTraceListener tr1 = new TextWriterTraceListener(System.Console.Out);
+                Debug.Listeners.Add(tr1);
+
+                TextWriterTraceListener tr2 = new TextWriterTraceListener(Settings.Config.Instance.LogFolder + "\\" + Settings.Config.Instance.DebugLogFile);
+                Debug.Listeners.Add(tr2);
+            }
+
+            Program prg = new Program();
+
+            if (Settings.Config.Instance != null)
+                configFound = true;
+
+            if (!configFound)
+            {
+                System.Console.WriteLine("\r\nCould not start server. There is a problem with the config file.");
+                Console.ReadKey(true);
+                Environment.Exit(0);
+            }
+
+            System.Console.WriteLine("* Debug: " + Settings.Config.Instance.Debug );
+            System.Console.WriteLine("* Logfile: '" + System.IO.Directory.GetCurrentDirectory().ToString() + "\\" + Settings.Config.Instance.LogFolder + "\\" + Settings.Config.Instance.DebugLogFile + "'");
+            System.Console.WriteLine("* Starting server [" + Settings.Config.Instance.ServerName + "] on port " + Settings.Config.Instance.ServerPort + "...");
+
+            
+
+            bool isListening = Server.UdpServer.Instance.startServer();
+
+            
+
+            if (isListening)
+            {
+                System.Console.WriteLine("* [" + Settings.Config.Instance.ServerName + "] is now listening @ " + Server.UdpServer.Instance.ServerAddress + "...");
+                System.Console.WriteLine("\r\nExit server with CTRL+X...");
+                System.Console.WriteLine();
+
+                Debug.WriteLineIf(Settings.Config.Instance.Debug, "Server running", DateTime.Now.ToString() + " " + prg.GetType().Name + ".Main");
+
+                bool noCTRLC = true;
+
+                while (noCTRLC)
+                {
+                    prg.cki = Console.ReadKey(true);
+
+                    if (prg.cki.Modifiers == ConsoleModifiers.Control && prg.cki.Key == ConsoleKey.X)
+                    {
+                        Server.UdpServer.Instance.stopServer();
+                        noCTRLC = false;
+
+                        System.Console.WriteLine("Server stopped...");
+                        System.Console.WriteLine("Exit.");
+                    }
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("Something went wrong...");
+                System.Console.WriteLine("Exit.");
+            }
+
+            Debug.Close();
+        }       
+    }
+}
