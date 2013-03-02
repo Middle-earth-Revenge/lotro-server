8ed5c2eaeee43a518022d3ebef76bf5b796829e1
bwgypyth <bwgypyth@gmx.net>
2013-03-02 14:58:34 +0100

    Move documentation to the class
    This way the IDE should be able to display it

--- LOTRO-Tools/Server/SocketObject.cs
+++ LOTRO-Tools/Server/SocketObject.cs
@@ -2,19 +2,15 @@
 using System.Net;
 using Protocol.Generic;
 
-/* Class SocketObject
- * 
- * Contains:
- * 
- * raw buffer [max. 65535 bytes]
- * client endpoint [eg. 127.0.0.1:56789]
- * serialized payload (the data payload of the dgram packet contains two elements - a header and a data part)
- * 
-*/
-
-
 namespace Server
 {
+    /// <summary>
+    /// Contains:
+    /// 
+    /// raw buffer [max. 65535 bytes]
+    /// client endpoint [eg. 127.0.0.1:56789]
+    /// serialized payload (the data payload of the dgram packet contains two elements - a header and a data part)
+    /// </summary>
     public class SocketObject
     {
         private const int bufferSize = 65535; // max 65535
