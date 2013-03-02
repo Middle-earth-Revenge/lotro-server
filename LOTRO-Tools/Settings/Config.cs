8ef73a576a90426c8542ed429ef4de987801048e
bwgypyth <bwgypyth@gmx.net>
2013-03-02 13:12:52 +0100

    Add settings project

---
+++ LOTRO-Tools/Settings/Config.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+
+
+/* 
+ 
+ * To disable checksum check at "3B 4C 24 1C 75 6A 8B"
+ * Replace 75 6a with 90 90
+ 
+ */
+
+namespace Settings
+{
+    //[DataContract]
+    [Serializable]
+    public sealed class Config
+    {
+        private static volatile Config instance;
+        private static object syncRoot = new Object();
+
+        internal Config()
+        {
+
+        }
+
+        public static Config Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new Config();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+
+            internal set { instance = value; }
+
+        }
+
+        internal void init()
+        {
+            if (this.Debug)
+            {
+                createFolder(LogFolder + "\\" + ClientLogFolder);
+                createFolder(LogFolder + "\\" + ServerLogFolder);
+            }
+
+            createFolder(AccountFolder);
+        }
+
+        private void createFolder(string folderName)
+        {
+            DirectoryInfo directoryInfo = Directory.CreateDirectory(@folderName);
+        }
+
+        [XmlElement("Debug")]
+        public bool Debug
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("DebugLogFile")]
+        public string DebugLogFile
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("LogFolder")]
+        public string LogFolder
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("ClientLogFolder")]
+        public string ClientLogFolder
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("ServerLogFolder")]
+        public string ServerLogFolder
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("RequiredClientVersion")]
+        public string RequiredClientVersion
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("ServerName")]
+        public string ServerName
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("ServerId")]
+        public UInt16 ServerId
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("ServerPort")]
+        public UInt16 ServerPort
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("WorldServerIP")]
+        public string WorldServerIP
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("WorldServerPort")]
+        public UInt16 WorldServerPort
+        {
+            get;
+            set;
+        }
+
+        [XmlElement("AccountFolder")]
+        public string AccountFolder
+        {
+            get;
+            set;
+        }
+
+    }
+}
---
+++ LOTRO-Tools/Settings/ConfigHandler.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml;
+
+namespace Settings
+{
+    public class ConfigHandler
+    {
+
+        public Settings.Config readConfig(string fileName)
+        {
+            XmlSerializer serializer = null;
+
+            try
+            {
+                serializer = new XmlSerializer(typeof(Settings.Config));
+
+                using (FileStream fs = new FileStream(@fileName, FileMode.Open))
+                {
+                    Settings.Config.Instance = (Settings.Config)serializer.Deserialize(fs);
+
+                    Settings.Config.Instance.init();
+                    
+                    fs.Close();
+                    fs.Dispose();
+                }
+
+                serializer = null;
+            }
+            catch (FileNotFoundException fnf)
+            {
+                Console.Write(fnf.Message);
+            }
+            catch (System.InvalidOperationException ioe)
+            {
+                Console.Write(ioe.Message);
+            }
+            catch (SerializationException se)
+            {
+                Console.Write(se.Message);
+            }
+
+            return Settings.Config.Instance;
+        }
+
+        // don't want to write a config
+        private void writeConfig(Settings.Config config)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(Settings.Config));
+
+            FileStream fs = new FileStream(@"config.xml", FileMode.Create);
+
+            XmlWriterSettings xws = new XmlWriterSettings();
+            xws.Indent = true;
+            xws.IndentChars = "\t";
+            
+            XmlWriter writer = XmlWriter.Create(fs,xws);
+
+            serializer.Serialize(writer, config);
+            
+            writer.Close();
+            fs.Close();
+            fs = null;
+            writer = null;          
+
+            serializer = null;
+        }
+    }
+}
