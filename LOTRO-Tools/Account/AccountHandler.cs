721a9af0a7691a709c415b6410011d81fcb6c218
bwgypyth <bwgypyth@gmx.net>
2013-03-02 13:15:51 +0100

    Add Account project

---
+++ LOTRO-Tools/Account/AccountHandler.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Diagnostics;
+
+namespace Account
+{
+    public class AccountHandler
+    {
+        private List<User> validAccounts = new List<User>();
+
+        public AccountHandler()
+        {
+            // in reality a account has to be authenticated through the webservice and then added to the list
+            // in this case we read "valid" accounts from a folder 
+
+        }
+
+        public User getUser(string accountName)
+        {
+            User user = parseUser(accountName);
+
+            return user;
+        }
+
+        private User parseUser(string accountName)
+        {
+            XmlSerializer serializer = null;
+            User user = null;
+
+            try
+            {
+                serializer = new XmlSerializer(typeof(Account.User));
+
+                using (FileStream fs = new FileStream(@Settings.Config.Instance.AccountFolder + "\\" + accountName + ".xml", FileMode.Open))
+                {
+                    user = (Account.User)serializer.Deserialize(fs);
+
+                    fs.Close();
+                    fs.Dispose();
+                }
+
+                serializer = null;
+            }
+            catch (FileNotFoundException fnf)
+            {
+                Debug.WriteIf(Settings.Config.Instance.Debug, fnf.Message, DateTime.Now.ToString() + " " + this.GetType().Name + ".GetUser");
+                Debug.Flush();
+            }
+            catch (System.InvalidOperationException ioe)
+            {
+                Debug.WriteIf(Settings.Config.Instance.Debug, ioe.Message, DateTime.Now.ToString() + " " + this.GetType().Name + ".GetUser");
+                Debug.Flush();
+            }
+            catch (SerializationException se)
+            {
+                Debug.WriteIf(Settings.Config.Instance.Debug, se.Message, DateTime.Now.ToString() + " " + this.GetType().Name + ".GetUser");
+                Debug.Flush();
+            }
+
+            return user;
+        }
+    }
+}
---
+++ LOTRO-Tools/Account/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Account
+{
+    [Serializable]
+    public class User
+    {
+        public User(string accountName)
+        {
+            this.AccountName = accountName;
+        }
+
+        public User()
+        {
+
+        }
+
+        [XmlElement("AccountName")]
+        public string AccountName { get; set; }
+
+        // Not needed for this stage of private server version
+        [XmlElement("Password")]
+        public string Password { get; set; }
+
+        [XmlElement("IPAddress")]
+        public string IPAddress { get; set; }
+
+        [XmlElement("ServerName")]
+        public string ServerName { get; set; }
+
+        // not needed for this stage of private server version
+        [XmlElement("SessionTicket")]
+        public string SessionTicket { get; set; }
+
+    }
+}
