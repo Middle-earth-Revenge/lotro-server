6c63e5eb8e866475b7eef977855fc9a4c02e6284
bwgypyth <bwgypyth@gmx.net>
2013-10-06 12:47:53 +0200

    Add project to document the behaviour of Helper.dll
    Sadly tAmMo decided to run confuser 1.9 on the dll, so we cannot
    decrypt it easily and we need to do interface changes to it. Next
    to the fact that porting the code away from C# is also intended.

---
+++ LOTRO-Tools/HelperAnalyzer/BEBinaryReaderTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HelperAnalyzer
+{
+    [TestClass]
+    public class BEBinaryReaderTests
+    {
+        [TestMethod]
+        public void testReadUInt16BE()
+        {
+            byte[] data = new byte[4];
+            for (int i = 0; i < data.Length; i++)
+            {
+                data[i] = (byte) (i+1);
+            }
+            MemoryStream stream = new MemoryStream(data);
+            Helper.BEBinaryReader reader = new Helper.BEBinaryReader(stream, Encoding.UTF8);
+
+            ushort tmp = reader.ReadUInt16BE();
+            Assert.AreEqual(258, tmp);
+            tmp = reader.ReadUInt16BE();
+            Assert.AreEqual(772, tmp);
+        }
+    }
+}
---
+++ LOTRO-Tools/HelperAnalyzer/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HelperAnalyzer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HelperAnalyzer")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca93e399-4a93-4cba-8c47-3bdadaabc388")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
